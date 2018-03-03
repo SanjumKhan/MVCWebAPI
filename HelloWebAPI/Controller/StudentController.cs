96b6a9cf8c29c4f00f3a68c916b3b05ec43b9398
sanjumk <sanjumk@L-442002757>
2018-03-03 08:51:04 +0530

    Add project files.

---
+++ HelloWebAPI/Controller/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HelloWebAPI.Controller
+{
+    public class StudentController : ApiController
+    {
+        public class Student
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+       // [Route("api/student/names")]
+        public IEnumerable<string> GetStudent()
+        {
+            return new string[] { "Ram", "Rahim" };
+        }
+        public List<Student> Post(Student student)
+        {
+            List<Student> lstStudent = new List<Student>();
+            lstStudent.Add(student);
+            return lstStudent;
+        }
+    }
+}
